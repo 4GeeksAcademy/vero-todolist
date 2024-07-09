8c59e7d92a2abe4b03442292d8c97103e48471c2
Vero519 <voviedo6@gmail.com>
2024-07-09 00:42:42 +0000

    tareas-primera-entrega

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	//declaracion de estados
+	let [tarea, setTarea] = useState("")
+	let [lista, setLista] = useState([])
+
+
+	const agregarTarea = () => {
+		if (tarea.trim() !== "") {
+			const nuevaLista = {
+				id: tarea.length + 1,
+				texto: tarea,
+				completada: false
+			};
+
+			setLista([...lista, nuevaLista]);
+			setTarea("");
+		}
+	}
+	const eliminarTarea = (id) => {
+		setLista(lista.filter(elemento => elemento.id !== id));
+
+	}
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+
+
+		<div className="container">
+			<div className="text-center">
+				<label htmlFor="inputValue" className="form-label fs-1 text-secondary">todos</label>
+				<div className="d-flex">
+					<input type="text" id="inputValue" className="form-control fs-3" value={tarea} onChange={(e) => setTarea(e.target.value)} onKeyDown={(e) => e.key == "Enter" && agregarTarea()} />
+					<button type="button" className="btn btn-primary" onClick={agregarTarea}>Agrgar a la lista</button>
+
+				</div>
+				<div id="passwordHelpBlock" className="form-text">
+					<ul>
+						{lista.map(item => (
+							<li key={item.id}>
+								<span >{item.texto}</span>
+								<button className="btn btn-ligth" onClick={() => eliminarTarea(item.id)}>x</button>
+
+							</li>
+						))}
+
+
+					</ul>
+				</div>
+
+			</div>
 		</div>
 	);
 };
